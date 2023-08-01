a32a633af5a13227a6e6a89e7c1f57ab5362c16c
Marat <111959077+maratsayfutdinov7@users.noreply.github.com>
2023-08-01 10:29:33 +0700

    Update autotest.js

--- autotest.js
+++ autotest.js
@@ -85,3 +85,24 @@ pm.test('Response body matches schema', function () {
 pm.test("Request is made using HTTPS", function () {
     pm.expect(pm.request.url.protocol).to.eql("https");
 });
+
+// GET http://send-request.me/api/companies/ 
+// Получение данных по незащищенному протоколу "http" 
+
+
+pm.test("Status code is 301", function () {
+    pm.response.to.have.status(301);
+});
+
+pm.test("Response time is less than 500ms", function () {
+    pm.expect(pm.response.responseTime).to.be.below(500);
+});
+
+pm.test("Headers is vaild", function() {
+pm.expect(pm.response.headers.get('Connection')).to.eql('keep-alive');
+pm.expect(pm.response.headers.get('Location')).to.eql('https://send-request.me/api/companies/');
+});
+
+pm.test("Request by http", function() {
+pm.expect(pm.request.url.protocol).to.eql("http")
+});
