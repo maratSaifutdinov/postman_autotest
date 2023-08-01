742c118d68c1cd3d7621522086bfee4615a7d6ca
Marat <111959077+maratsayfutdinov7@users.noreply.github.com>
2023-08-01 10:04:33 +0700

    Update autotest.js

--- autotest.js
+++ autotest.js
@@ -1,30 +1,87 @@
-// autotest for method api.hh.ru/employers/5008932
+// GET {{baseUrl}}/api/companies
 
-
-pm.test("Мой первый автотест", function () {
+pm.test("Status code is 200", function () {
     pm.response.to.have.status(200);
 });
 
-pm.test("Body matches string Яндекс.Практикум", function () {
-    pm.expect(pm.response.text()).to.include("Яндекс.Практикум");
+pm.test("Response time is less than 500ms", function () {
+    pm.expect(pm.response.responseTime).to.be.below(500);
 });
 
-pm.test("Response time is less than 50ms", function () {
-    pm.expect(pm.response.responseTime).to.be.below(50);
+pm.test("Headers are valid", function () {
+    pm.expect(pm.response.headers.get('Content-Type')).to.eql('application/json');
+    pm.expect(pm.response.headers.get('Connection')).to.eql('keep-alive');
 });
 
+let DataJson = pm.response.json().data;
 
-//autotest for method https://api.hh.ru/employers?text=яндекс+практикум
-
-pm.test("Мой первый автотест", function () {
-    pm.response.to.have.status(200);
+pm.test("Length of JSON with default limit", function () {
+    let countLenData = Object.keys(DataJson).length;
+    pm.expect(countLenData).to.eql(3);
 });
 
-pm.test("Body matches string Яндекс.Практикум", function () {
-    pm.expect(pm.response.text()).to.include("Яндекс.Практикум");
+pm.test("Verify offset", function () {
+    pm.expect(DataJson[0].company_id).to.eql(1);
 });
 
-pm.test("Response time is less than 50ms", function () {
-    pm.expect(pm.response.responseTime).to.be.below(50);
+let schema = {
+    "type": "object",
+    "properties": {
+        "data": {
+            "type": "array",
+            "items": {
+                "type": "object",
+                "properties": {
+                    "company_id": {
+                        "type": "integer"
+                    },
+                    "company_name": {
+                        "type": "string"
+                    },
+                    "company_address": {
+                        "type": "string"
+                    },
+                    "company_status": {
+                        "type": "string",
+                        "enum": ["ACTIVE", "CLOSED", "BANKRUPT"]
+                    }
+                },
+                "required": [
+                    "company_id",
+                    "company_name",
+                    "company_address",
+                    "company_status"
+                ]
+            }
+        },
+        "meta": {
+            "type": "object",
+            "properties": {
+                "limit": {
+                    "type": "integer"
+                },
+                "offset": {
+                    "type": "integer"
+                },
+                "total": {
+                    "type": "integer"
+                }
+            },
+            "required": [
+                "total"
+            ]
+        }
+    },
+    "required": [
+        "data",
+        "meta"
+    ]
+};
+
+pm.test('Response body matches schema', function () {
+    pm.response.to.have.jsonSchema(schema);
 });
 
+pm.test("Request is made using HTTPS", function () {
+    pm.expect(pm.request.url.protocol).to.eql("https");
+});
