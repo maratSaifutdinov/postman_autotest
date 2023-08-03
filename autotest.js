2d3155418b4025f7a762427b6b1c0c474f028c69
Marat <111959077+maratsayfutdinov7@users.noreply.github.com>
2023-08-03 09:42:54 +0700

    Update autotest.js

--- autotest.js
+++ autotest.js
@@ -772,3 +772,95 @@ let schema = {
 pm.test('Schema is valid', function() {
 pm.response.to.have.jsonSchema(schema);
 });
+
+// GET {{baseUrl}}/api/issues/companies?status=ACTIVE
+
+pm.test("Status code is 200", function () {
+    pm.response.to.have.status(200);
+});
+
+pm.test("Response time is less than 500ms", function () {
+    pm.expect(pm.response.responseTime).to.be.below(500);
+});
+
+pm.test("Headers is valid", function() {
+pm.expect(pm.response.headers.get('Content-Type')).to.eql('application/json');
+pm.expect(pm.response.headers.get('Connection')).to.eql('keep-alive')
+});
+
+pm.test("Verify status ACTIVE", function(){
+    for(let company of pm.response.json().data){
+    pm.expect(company.company_status).to.be.eql("ACTIVE");
+    }
+});
+
+let DataJson = pm.response.json().data;
+
+pm.test("Length of JSON with default limit", function () {
+    let countLenData = Object.keys(DataJson).length;
+    pm.expect(countLenData).to.eql(3);
+});
+
+pm.test("Verify offset", function () {
+    pm.expect(DataJson[0].company_id).to.eql(1);
+});
+
+let schema = {
+    "type": "object",
+    "properties": {
+        "data": {
+            "type": "array",
+            "items": {
+                "type": "object",
+                "properties": {
+                    "company_id": {
+                        "type": "integer"
+                    },
+                    "company_name": {
+                        "type": "string"
+                    },
+                    "company_address": {
+                        "type": "string"
+                    },
+                    "company_status": {
+                        "type": "string",
+                        "enum": ["ACTIVE"]
+                    }
+                },
+                "required": [
+                    "company_id",
+                    "company_name",
+                    "company_address",
+                    "company_status"
+                ]
+            }
+        },
+        "meta": {
+            "type": "object",
+            "properties": {
+                "limit": {
+                    "type": "integer"
+                },
+                "offset": {
+                    "type": "integer"
+                },
+                "total": {
+                    "type": "integer"
+                }
+            },
+            "required": [
+                "total"
+            ]
+        }
+    },
+    "required": [
+        "data",
+        "meta"
+    ]
+};
+
+pm.test('Response body matches schema', function () {
+    pm.response.to.have.jsonSchema(schema);
+});
+
+
