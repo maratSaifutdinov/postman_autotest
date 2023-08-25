499f83859e7ad6c7b0b780e9fd31619636ad4911
Marat <111959077+maratsayfutdinov7@users.noreply.github.com>
2023-08-25 16:01:11 +0700

    update-pm_tests.js

--- pm_tests.js
+++ pm_tests.js
@@ -78,3 +78,36 @@ let schema = JSON.parse(pm.environment.get('notFound'));
 pm.test('Schema is valid', function () {
     pm.response.to.have.jsonSchema(schema);
 });
+
+// GET {{base_url}}/api/{{api_version}}/product/article/{{article}} | Получение основной информации о товаре по артикулу
+
+pm.test("Status code is 200", function () {
+    pm.response.to.have.status(200);
+});
+
+pm.test("Status code name has string", () => {
+    pm.response.to.have.status("OK");
+});
+
+pm.test("Response time is less than 500ms", function () {
+    pm.expect(pm.response.responseTime).to.be.below(500);
+});
+
+pm.test("Request is made using HTTPS", function () {
+    pm.expect(pm.request.url.protocol).to.eql("https");
+});
+
+pm.test("Headers are valid", function () {
+    pm.expect(pm.response.headers.get('Content-Type')).to.eql('application/json; charset=utf-8');
+    pm.expect(pm.response.headers.get('Connection')).to.eql('keep-alive');
+});
+
+pm.test('Test Cookie', function () {
+    pm.expect(pm.cookies.has("PHPSESSID")).to.be.true;
+});
+
+let schema = JSON.parse(pm.environment.get("InformationArticle"));
+
+pm.test('Schema is valid', function () {
+    pm.response.to.have.jsonSchema(schema);
+});
