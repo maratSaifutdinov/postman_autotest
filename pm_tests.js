f417a26288bfac07128e11e171c81ac3add4e774
Marat <111959077+maratsayfutdinov7@users.noreply.github.com>
2023-08-28 12:44:08 +0700

    update-pm_tests

--- pm_tests.js
+++ pm_tests.js
@@ -111,3 +111,33 @@ let schema = JSON.parse(pm.environment.get("InformationArticle"));
 pm.test('Schema is valid', function () {
     pm.response.to.have.jsonSchema(schema);
 });
+
+// GET {{base_url}}/api/{{api_version}}/product/article/{{nonExistentArticle}} | Получение информации о товаре по несуществующему артикулу
+
+pm.test("Status code is 404", function () {
+    pm.response.to.have.status(404);
+});
+
+pm.test("Status code name has string", () => {
+    pm.response.to.have.status("Not Found");
+});
+
+pm.test("Response time is less than 500ms", function () {
+    pm.expect(pm.response.responseTime).to.be.below(500);
+});
+
+pm.test("Headers are valid", function () {
+    pm.expect(pm.response.headers.get('Content-Type')).to.eql('application/json; charset=utf-8');
+    pm.expect(pm.response.headers.get('Connection')).to.eql('keep-alive');
+});
+
+pm.test('Test Cookie', function () {
+    pm.expect(pm.cookies.has("PHPSESSID")).to.be.true;
+});
+
+let schema = JSON.parse(pm.environment.get("notFound"));
+
+pm.test('Schema is valid', function () {
+    pm.response.to.have.jsonSchema(schema);
+});
+
